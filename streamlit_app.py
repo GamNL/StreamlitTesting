a8816d5b557c747bacf207eb6aa6ac6eaf1e0df6
Nino <n.lauriks@live.com>
2021-11-29 17:55:58 +0100

    Basis neerslag verwerking

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import requests 
 import geocoder
+import plotly.graph_objects as go
+
+
+def verwerk_neerslag(sWeer):
+    lRows = sWeer.split("\n")
+    dRet = {"Tijd":[],"Neerslag":[]}
+
+    for sRow in lRows :
+        lRes = sRow.split("|")
+        dRet["Tijd"].append(lRes[1])
+        dRet["Neerslag"].append(lRes[0])
+
+    return dRet
+
+    
+
 
 st.markdown("# Basic buien radar")
 sLocation = st.text_input("Plaats:","Amsterdam")
@@ -11,4 +27,5 @@ st.markdown("Locatie lat long: {}".format(lLocation))
 resp = requests.get("https://gpsgadget.buienradar.nl/data/raintext?lat=%s&lon=%s" 
         % (lLocation[0], lLocation[1]))
 st.markdown("## Weather info")
-st.text(resp.text)
+dData = verwerk_neerslag(resp.text)
+st.write(dData)
