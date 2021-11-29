bf6561bdcb5fe1c324bf94539eeaa9ceb08d0d1d
Nino <n.lauriks@live.com>
2021-11-29 17:40:08 +0100

    Weather data for amsterdam

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import geocoder
 
 st.markdown("# Hello streamlit")
-lLocation = geocoder.ip("me").latlng
+lLocation = geocoder.google("Amsterdam").latlng
 st.markdown("Your location: {}".format(lLocation))
 
 resp = requests.get("https://gpsgadget.buienradar.nl/data/raintext?lat=%s&lon=%s" 
