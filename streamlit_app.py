a82cc42fbebdd14c96e7bde641727763be62738d
Nino <n.lauriks@live.com>
2021-11-29 17:46:36 +0100

    OSM

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import geocoder
 
 st.markdown("# Hello streamlit")
-lLocation = geocoder.google("Amsterdam, Nederland")
+lLocation = geocoder.osm("Amsterdam, Nederland")
 st.markdown("Your location: {}".format(lLocation))
 
 resp = requests.get("https://gpsgadget.buienradar.nl/data/raintext?lat=%s&lon=%s" 
