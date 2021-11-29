baa56066112001527c53140bfc9a0733e1209e85
Nino <n.lauriks@live.com>
2021-11-29 17:50:35 +0100

    Text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit as st
 import requests 
 import geocoder
 
-st.markdown("# Hello streamlit")
-lLocation = geocoder.osm("Amsterdam").latlng
-st.markdown("Your location: {}".format(lLocation))
+st.markdown("# Basic buien radar")
+sLocation = st.text_input("Plaats:","Amsterdam")
+
+lLocation = geocoder.osm(sLocation).latlng
+st.markdown("Locatie lat long: {}".format(lLocation))
 
 resp = requests.get("https://gpsgadget.buienradar.nl/data/raintext?lat=%s&lon=%s" 
         % (lLocation[0], lLocation[1]))
