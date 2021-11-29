ddb4c95b265e7b46bc0398ad819acde4269ecaaa
Nino <n.lauriks@live.com>
2021-11-29 17:38:12 +0100

    Buienrader

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.markdown("# Hello streamlit")
 lLocation = geocoder.ip("me").latlng
 st.markdown("Your location: {}".format(lLocation))
 
-resp = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&exclude=hourly,daily&appid=%s&units=imperial" 
-        % (lLocation[0], lLocation[1], "43eda1fbcf0b07043eeeac93ae54e1e0"))
+resp = requests.get("https://gpsgadget.buienradar.nl/data/raintext?lat=%s&lon=%s" 
+        % (lLocation[0], lLocation[1]))
 st.markdown("## Weather info")
 st.write(resp.text)
