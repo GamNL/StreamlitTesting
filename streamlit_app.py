defbada5699575b35afc3024da7c5d990004b4a2
Nino <n.lauriks@live.com>
2021-11-29 17:29:53 +0100

    fix for \

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import requests 
 import geocoder
 
-st.markdown("# Hello streamlit")\
-lLocation =  geocoder.ip("me").latlng
-st.markdown("Your location: {}".format(geocoder.ip("me").latlng))
+st.markdown("# Hello streamlit")
+lLocation = geocoder.ip("me").latlng
+st.markdown("Your location: {}".format(lLocation))
 
 resp = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&exclude=hourly,daily&appid=%s&units=imperial" 
         % (lLocation[0], lLocation[1], "43eda1fbcf0b07043eeeac93ae54e1e0"))
